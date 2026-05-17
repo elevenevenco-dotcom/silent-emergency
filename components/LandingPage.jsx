657847b47c7d6b3a3de32682a3098064e04cad4d
elevenevenco-dotcom <elevenevenco@gmail.com>
2026-05-17 16:04:56 -0500

    Update LandingPage.jsx

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -237,8 +237,8 @@ export default function LandingPage({ onGetStarted }) {
           <span style={{ fontFamily: 'Syne', fontWeight: 700, fontSize: 14, color: COLORS.text }}>Silent Emergency</span>
         </div><p style={{ fontSize: 12, color: COLORS.textDim }}>© 2025 Silent Emergency Inc. · Privacy Policy · Terms of Use</p>
 
-</div>
-</div>
-)
+        </div>
+        </div>
+  );
 }
   
