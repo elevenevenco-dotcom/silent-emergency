37eb6454eaf8e566b70048b1a9c82af68cbcb111
elevenevenco-dotcom <elevenevenco@gmail.com>
2026-05-17 12:43:00 -0500

    Update LandingPage.jsx

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -103,8 +103,8 @@ export default function LandingPage({ onGetStarted }) {
               </div>
               </div>
               ))}
-              
-        </a>
+              </a>
+       
       
     
 
